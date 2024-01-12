103c9fda5302c0aee628c91ef0f7873db5b5d82c
saymon005 <msaymonislamiftikar@gmail.com>
2024-01-13 00:15:29 +0600

    Day 1

--- 2_Best Time to Buy and Sell Stock.cs
+++ 2_Best Time to Buy and Sell Stock.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Leetcode_Blind_75
 {
-    internal class Best_Time_to_Buy_and_Sell_Stock
+    public class Best_Time_to_Buy_and_Sell_Stock
     {
+        public static void main(string[] args)
+        {
+            int size = Convert.ToInt32(Console.ReadLine());
+            string[] inputArray = Console.ReadLine().Split(' ');
+            int[] prices = Array.ConvertAll(inputArray, int.Parse);
+            int mxProfit = 0;
+            int minPrice = prices[0];
+
+            for(int i=0; i<prices.Length; i++)
+            {
+                minPrice = Math.Min(minPrice, prices[i]);
+                int profit = Math.Abs(prices[i]- minPrice);
+                mxProfit = Math.Max(profit, mxProfit);
+            }
+
+            Console.WriteLine(mxProfit);
+            Console.ReadLine();
+        }
     }
 }
