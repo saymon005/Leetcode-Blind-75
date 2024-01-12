a7594ba6646b1fc64c459dd2a3d0ad8f0bfc7e98
saymon005 <msaymonislamiftikar@gmail.com>
2024-01-13 00:29:44 +0600

    Day 1

---
+++ 3_Contains Duplicate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_Blind_75
+{
+    public class _3_Contains_Duplicate
+    {
+        public static void Main(string[] args)
+        {
+            int size = Convert.ToInt32(Console.ReadLine()); 
+            string[] inputArray = new string[size];
+            int[] nums = Array.ConvertAll(inputArray, int.Parse);
+            HashSet<int> s = new HashSet<int>();
+            for(int i=0; i< nums.Length; i++)
+            {
+                s.Add(nums[i]);
+            }
+
+            if(s.Count == nums.Length)
+            {
+                Console.WriteLine("False");
+            }
+            else
+            {
+                Console.WriteLine("True");
+            }
+        }
+    }
+}
